57da8bba0cb94b368fb436cb0139c88705b94afd
Jesus Coronado <jesus@nytt.com.ar>
2018-05-22 23:21:00 -0300

    reseteado de angular

--- www/src/app/app.component.ts
+++ www/src/app/app.component.ts
@@ -6,10 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title  = 'Puma webpage';
-  description = 'proyecto para puma hecho por mí';
-  folder = {
-  	name: 'nombre de la carpeta',
-  	update: 'el otro día'
-  };
+  title = 'app';
 }
--- www/src/app/app.module.ts
+++ www/src/app/app.module.ts
@@ -1,34 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+
 import { AppComponent } from './app.component';
-import { MaterialModule } from './material/material.module';
-
-import { AppRoutesModule } from './app-routes.module';
-
-import { NavbarComponent } from './navbar/navbar.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { HomepageComponent } from './homepage/homepage.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavbarComponent,
-    HeaderComponent,
-    FooterComponent,
-    HomepageComponent,
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    MaterialModule,
-    AppRoutesModule,
+    BrowserModule
   ],
   providers: [],
-  bootstrap: [
-    AppComponent,
-    NavbarComponent,
-    HeaderComponent,
-    FooterComponent,
-  ]
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
